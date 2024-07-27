dff4c0d93f01acd5750a939bb2b24a8898234995
Nekoza <nguyenminhhoangvu2005@gmail.com>
2024-07-27 08:36:34 +0700

    Complete Styling

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const API_KEY = '8edc9ad0d1add16511f788aca04bcf2d';
 const GEO_API_URL = 'http://api.openweathermap.org/geo/1.0/direct';
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded( {extended: true} ))
 
 app.get('/', (req, res) => {
@@ -15,10 +16,14 @@ app.get('/', (req, res) => {
 })
 
 app.post('/submit', async (req, res) => {
-    const coordinates = await getCoordinates(req.body.city);
-    const weather = await getWeather(coordinates);
-
-    res.render('index.ejs', { data: weather })
+    
+    try {
+        const coordinates = await getCoordinates(req.body.city);
+        const weather = await getWeather(coordinates);
+        res.render('index.ejs', { data: weather })
+    } catch (error) {
+        res.render('index.ejs', {error: 'Location is not available' })
+    }
 })
 
 
@@ -27,31 +32,38 @@ app.listen(port, () => {
 })
 
 async function getCoordinates ( city ) {
-    const config = { params: { q: city, appid: API_KEY } }
-    const response = await axios.get(GEO_API_URL, config);
-    const result = response.data;
+    try {
+        const config = { params: { q: city, appid: API_KEY } }
+        const response = await axios.get(GEO_API_URL, config);
+        const result = response.data;
 
-    const coordinates = {
-        longitude: result[0].lon,
-        latitude: result[0].lat
-    }
+        const coordinates = {
+            longitude: result[0].lon,
+            latitude: result[0].lat
+        }
 
-    return coordinates;
+        return coordinates;
+
+    } catch (error) { throw error }
+    
 }
 
 async function getWeather ( coordinates ) {
-    const config = { params: { lat: coordinates.latitude, lon: coordinates.longitude, appid: API_KEY } }
-    const response = await axios.get(WEATHER_API_URL, config);
-    const result = response.data;
-    
-    const details = {
-        weather: {
-            main: result.weather[0].main,
-            description: result.weather[0].description
-        },
-        temp: result.main.temp,
-        humidity: result.main.humidity
-    }
+    try {
+        const config = { params: { lat: coordinates.latitude, lon: coordinates.longitude, appid: API_KEY } }
+        const response = await axios.get(WEATHER_API_URL, config);
+        const result = response.data;
+        
+        const details = {
+            weather: {
+                main: result.weather[0].main,
+                description: result.weather[0].description
+            },
+            temp: result.main.temp,
+            humidity: result.main.humidity
+        }
 
-    return details;
+        return details;
+        
+    } catch (error) { throw error }
 }
